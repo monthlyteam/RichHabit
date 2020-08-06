cd7c3e6332bcfc25530a6011e86ce24cdba9f976
Ryusim <bkryusim@gmail.com>
2020-08-06 16:23:36 +0900

    Switching SliverAppBar, CardList in Invest screen, fixed AppBar and Markdown in InvestDetail screen

--- rich_habit/lib/main.dart
+++ rich_habit/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:richhabit/screens/init.dart';
-import 'package:richhabit/screens/invest.dart';
 import 'main_page.dart';
 
 void main() {
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'RichHabit',
       theme: ThemeData(),
-      home: Invest(),
+      home: MainPage(),
       debugShowCheckedModeBanner: false,
     );
   }
---
+++ rich_habit/lib/screens/invest_detail.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:richhabit/constants.dart';
+
+class InvestDetail extends StatelessWidget {
+  final title;
+  final mdpath;
+
+  InvestDetail({Key key, @required this.title, this.mdpath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final markdownController = ScrollController();
+    return Scaffold(
+        appBar: AppBar(
+            title: Text(
+              title,
+              style:
+                  TextStyle(color: kPurpleColor, fontSize: kSubTitleFontSize),
+            ),
+            backgroundColor: kIvoryColor,
+            elevation: 0.0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              color: kPurpleColor,
+              iconSize: kSubTitleFontSize,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )),
+        body: FutureBuilder<String>(
+          future: rootBundle.loadString(mdpath),
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              print(snapshot.data);
+              return SafeArea(
+                child: Container(
+                    color: kIvoryColor,
+                    child: Markdown(
+                      data: snapshot.data,
+                      controller: markdownController,
+                      selectable: true,
+                      shrinkWrap: false,
+                    )),
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ));
+  }
+}
