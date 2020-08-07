45a8ce83373706a7595dd8ed7f81a9525b0d2e3b
허석민 <gjtjrals0@gmail.com>
2020-08-07 20:14:18 +0900

    habit.dart, user_provider -Done

--- rich_habit/lib/habit.dart
+++ rich_habit/lib/habit.dart
@@ -1,1 +1,33 @@
-class Habit {}
+import 'package:flutter/cupertino.dart';
+
+class Habit {
+  String name;
+  String iconURL;
+  double price; //습관 1회당 가격
+
+  int warningCount = 0; //경고 누적 수
+
+  bool usualIsWeek; // 평상시 횟수 단위 Daily = false, Weekly = true
+  int usualAmount; //평상시 습관 양
+
+  bool goalIsWeek; // 목표 횟수 단위
+  int goalAmount; // 목표 습관 양
+
+  int nowAmount = 0; // 현재 습관 양
+
+  Habit({
+    @required this.name,
+    @required this.iconURL,
+    @required this.price,
+    @required this.usualIsWeek,
+    @required this.usualAmount,
+    @required this.goalIsWeek,
+    @required this.goalAmount,
+  });
+
+  double get retention //목표 유지율
+      => ((usualAmount - nowAmount) / (usualAmount - goalAmount)) * 100.0;
+
+  double get saveMoney //절약 금액
+      => (usualAmount - nowAmount) * price;
+}
--- rich_habit/lib/user_provider.dart
+++ rich_habit/lib/user_provider.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
-class UserProvider with ChangeNotifier {}
+class UserProvider with ChangeNotifier {
+  bool isLogin = false;
+
+  String id = '';
+  String userName = '';
+  String title = '';
+  String profileURL = '';
+
+  bool isAlarm = false;
+  DateTime pushAlarmTime;
+
+  String language = 'KO';
+}
