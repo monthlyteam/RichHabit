91944dc4dfe91e58e5181ee9977f3fb6276269f2
허석민 <gjtjrals0@gmail.com>
2020-08-11 17:56:17 +0900

    Hot Fix Provider

--- rich_habit/lib/habit_provider.dart
+++ rich_habit/lib/habit_provider.dart
@@ -10,12 +10,55 @@ class HabitProvider with ChangeNotifier {
   Map<DateTime, List<Habit>> triggerHabit = {};
   Map<DateTime, List<Habit>> dailyHabit = {};
   Map<int, List<Habit>> weeklyHabit = {};
-  Map<DateTime, List<int>> warningCount = {}; //달력에 표시되는 아이콘
+  Map<DateTime, List<int>> calendarIcon = {}; //달력에 표시되는 아이콘
 
   HabitProvider() {
     var now = DateTime.now();
     nowDate = DateTime(now.year, now.month, now.day);
     nowWeekOfYear = (now.year * 100) + isoWeekNumber(now);
+
+    initHabit();
+  }
+
+  //날짜 확인후 처음 들어오면 이전 습관 틀 제공
+  void initHabit() {
+    var dailyKeys = dailyHabit.keys.toList();
+    var weeklyKeys = weeklyHabit.keys.toList();
+
+    DateTime lastDate = dailyKeys.last;
+    int lastNowWeekOfYear = weeklyKeys.last;
+    Habit initHabit;
+
+    List<Habit> initTriggerHabitList = [];
+    triggerHabit[lastDate].forEach((element) {
+      initHabit = element;
+      initHabit.nowAmount = 0;
+      initTriggerHabitList.add(initHabit);
+    });
+
+    List<Habit> initDailyHabitList = [];
+    dailyHabit[lastDate].forEach((element) {
+      initHabit = element;
+      initHabit.nowAmount = 0;
+      initDailyHabitList.add(initHabit);
+    });
+
+    List<Habit> initWeeklyHabitList = [];
+    weeklyHabit[lastNowWeekOfYear].forEach((element) {
+      initHabit = element;
+      initHabit.nowAmount = 0;
+      initWeeklyHabitList.add(initHabit);
+    });
+
+    for (int i = 0; lastDate.add(Duration(days: i)) != nowDate; i++) {
+      triggerHabit[lastDate.add(Duration(days: i + 1))] = initTriggerHabitList;
+      dailyHabit[lastDate.add(Duration(days: i + 1))] = initDailyHabitList;
+      calendarIcon[lastDate.add(Duration(days: i + 1))] = [0];
+    }
+
+    for (int i = 0; lastNowWeekOfYear + i != nowWeekOfYear; i++) {
+      weeklyHabit[lastNowWeekOfYear + 1] = initWeeklyHabitList;
+    }
   }
 
   void addHabit(Habit newHabit) {
@@ -82,14 +125,92 @@ class HabitProvider with ChangeNotifier {
     } else {
       //주기가 바뀐거 경우라면(Daily -> Weekly)
       int thisWeek = nowDate.weekday;
-
+      // TODO: 1.Modify Cycle Daily to Weekly
       for (int i = 0; i > thisWeek; i++) {}
     }
 
     notifyListeners();
   }
 
-  //이번달 예상 절약 금액(전부 null이면 전체)
+  void plusNowAmount(DateTime inputTime, DateTime addedTimeID, bool goalIsWeek,
+      bool isTrigger) {
+    if (isTrigger) {
+      int index = triggerHabit[inputTime]
+          .indexWhere((element) => element.addedTimeID == addedTimeID);
+
+      triggerHabit[inputTime][index].nowAmount++;
+    } else {
+      if (goalIsWeek) {
+        int isoWeekOfYear = isoWeekNumber(inputTime);
+        //weekly
+        int index = weeklyHabit[isoWeekOfYear]
+            .indexWhere((element) => element.addedTimeID == addedTimeID);
+
+        weeklyHabit[isoWeekOfYear][index].nowAmount++;
+        if (weeklyHabit[isoWeekOfYear][index].nowAmount >
+            weeklyHabit[isoWeekOfYear][index].goalAmount) {
+          calendarIcon[inputTime] = [2];
+        } else {
+          calendarIcon[inputTime] = [1];
+        }
+      } else {
+        //daily
+        int index = dailyHabit[inputTime]
+            .indexWhere((element) => element.addedTimeID == addedTimeID);
+
+        dailyHabit[inputTime][index].nowAmount++;
+        if (dailyHabit[inputTime][index].nowAmount >
+            dailyHabit[inputTime][index].goalAmount) {
+          calendarIcon[inputTime] = [2];
+        } else {
+          calendarIcon[inputTime] = [1];
+        }
+      }
+    }
+
+    notifyListeners();
+  }
+
+  void minusNowAmount(DateTime inputTime, DateTime addedTimeID, bool goalIsWeek,
+      bool isTrigger) {
+    if (isTrigger) {
+      int index = triggerHabit[inputTime]
+          .indexWhere((element) => element.addedTimeID == addedTimeID);
+
+      triggerHabit[inputTime][index].nowAmount--;
+    } else {
+      if (goalIsWeek) {
+        int isoWeekOfYear = isoWeekNumber(inputTime);
+        //weekly
+        int index = weeklyHabit[isoWeekOfYear]
+            .indexWhere((element) => element.addedTimeID == addedTimeID);
+
+        weeklyHabit[isoWeekOfYear][index].nowAmount--;
+        if (weeklyHabit[isoWeekOfYear][index].nowAmount >
+            weeklyHabit[isoWeekOfYear][index].goalAmount) {
+          calendarIcon[inputTime] = [2];
+        } else {
+          calendarIcon[inputTime] = [1];
+        }
+      } else {
+        //daily
+        int index = dailyHabit[inputTime]
+            .indexWhere((element) => element.addedTimeID == addedTimeID);
+
+        dailyHabit[inputTime][index].nowAmount--;
+        if (dailyHabit[inputTime][index].nowAmount >
+            dailyHabit[inputTime][index].goalAmount) {
+          calendarIcon[inputTime] = [2];
+        } else {
+          calendarIcon[inputTime] = [1];
+        }
+      }
+    }
+
+    notifyListeners();
+  }
+
+  //이번달 예상 절약 금액(addedTimeID null이면 => 전체)
   double thisMonthExpected(DateTime addedTimeID, bool goalIsWeek) {
     int maxDayOfMonth = DateTime(nowDate.year, nowDate.month + 1, 1)
         .subtract(Duration(days: 1))
@@ -138,37 +259,79 @@ class HabitProvider with ChangeNotifier {
     return money;
   }
 
-  //누적 절약 금액 계산
-  double cumSaving() {
+  //누적 절약 금액 계산(addedTimeID null이면 => 전체)
+  double cumSaving(DateTime addedTimeID) {
     double sum = 0;
-    weeklyHabit.forEach((key, value) {
-      value.forEach((element) {
-        sum += element.saveMoney;
+    if (addedTimeID == null) {
+      weeklyHabit.forEach((key, value) {
+        value.forEach((element) {
+          sum += element.saveMoney;
+        });
       });
-    });
 
-    dailyHabit.forEach((key, value) {
-      value.forEach((element) {
-        sum += element.saveMoney;
+      dailyHabit.forEach((key, value) {
+        value.forEach((element) {
+          sum += element.saveMoney;
+        });
       });
-    });
+    } else {
+      dailyHabit.forEach((key, value) {
+        value.forEach((element) {
+          if (element.addedTimeID == addedTimeID) {
+            sum += element.saveMoney;
+          }
+        });
+      });
+
+      weeklyHabit.forEach((key, value) {
+        value.forEach((element) {
+          if (element.addedTimeID == addedTimeID) {
+            sum += element.saveMoney;
+          }
+        });
+      });
+    }
+
     return sum;
   }
 
-  //해당 DateTime key값이 없으면 '물음표' 로 간주
-  void onWarning() {
-    if (warningCount.containsKey(nowDate)) {
-      warningCount[nowDate][0]++;
-    } else {
-      warningCount[nowDate][0] = 1;
-    }
+  //총 누적 절약금액의 절약 기록 표시(addedTimeID null이면 => 전체)
+  Map<dynamic, dynamic> showCumSavingHistory(DateTime addedTimeID) {
+    Map<int, Map<int, Map<int, List<String>>>> map = {};
+    // TODO: 2.Show cumSavingHistory
 
-    notifyListeners();
+    return map;
   }
 
-  //총 누적 절약금액의 절약 기록 표시
-  void showCumSavingHistory() {
-    Map<int, Map<int, Map<int, List<String>>>> map = {};
+  //목표 유지율 표시(addedTimeID null이면 => 전체)
+  List<double> getRetention(DateTime addedTimeID) {
+    // TODO : 3.getRetention
+    Map<int, double> retentions = {};
+    if (addedTimeID == null) {
+      int weeks = 0;
+      double avg = 0;
+      int len = 0;
+      weeklyHabit.forEach((key, value) {
+        value.forEach((element) {
+          avg = 0;
+          avg += element.retention;
+        });
+        len = value.length;
+        retentions[weeks] = avg / len;
+        weeks++;
+      });
+
+      weeks = 0;
+      avg = 0;
+      double dailyAvg = 0;
+      dailyHabit.forEach((key, value) {
+        value.forEach((element) {
+          dailyAvg = 0;
+          dailyAvg += element.retention;
+        });
+      });
+    } else {}
+    return null;
   }
 
   int isoWeekNumber(DateTime date) {
