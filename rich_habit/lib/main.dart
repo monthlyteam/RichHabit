a6807fa49b3b0ec04ba75918c8d01ab963c572ef
허석민 <gjtjrals0@gmail.com>
2020-08-17 22:00:02 +0900

    Test print in main

--- rich_habit/lib/main.dart
+++ rich_habit/lib/main.dart
@@ -11,21 +11,70 @@ import 'main_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  /*
-  addedTimeID: json['addedTimeID'],
-        name: json['name'],
-        iconURL: json['iconURL'],
-        price: json['price'],
-        usualIsWeek: json['usualIsWeek'],
-        usualAmount: json['usualAmount'],
-        goalIsWeek: json['goalIsWeek'],
-        goalAmount: json['goalAmount'],
-        nowAmount: json['nowAmount'],
-   */
+
+  Map<DateTime, List<Habit>> dailyHabit = Map<DateTime, List<Habit>>();
+
   var now = DateTime.now();
-  Habit test = Habit(
-    addedTimeID: now,
-  );
+  Habit test1 = Habit(
+      addedTimeID: now,
+      name: 'Habit1',
+      iconURL: 'assets/images/icon/custom_coin.svg',
+      price: 1000,
+      usualIsWeek: false,
+      usualAmount: 5,
+      goalIsWeek: false,
+      goalAmount: 1,
+      nowAmount: 0);
+  Habit test2 = Habit(
+      addedTimeID: now,
+      name: 'Habit2',
+      iconURL: 'assets/images/icon/custom_coin.svg',
+      price: 1000,
+      usualIsWeek: false,
+      usualAmount: 5,
+      goalIsWeek: false,
+      goalAmount: 1,
+      nowAmount: 0);
+  Habit test3 = Habit(
+      addedTimeID: now,
+      name: 'Habit3',
+      iconURL: 'assets/images/icon/custom_coin.svg',
+      price: 1000,
+      usualIsWeek: false,
+      usualAmount: 5,
+      goalIsWeek: false,
+      goalAmount: 1,
+      nowAmount: 0);
+  Habit test4 = Habit(
+      addedTimeID: now,
+      name: 'Habit4',
+      iconURL: 'assets/images/icon/custom_coin.svg',
+      price: 1000,
+      usualIsWeek: false,
+      usualAmount: 5,
+      goalIsWeek: false,
+      goalAmount: 1,
+      nowAmount: 0);
+
+  dailyHabit[DateTime(now.year, now.month, now.day)] = [test1];
+  dailyHabit[DateTime(now.year, now.month, now.day)].add(test2);
+  dailyHabit[DateTime(now.year, now.month, now.day).add(Duration(days: 1))] = [
+    test3
+  ];
+  dailyHabit[DateTime(now.year, now.month, now.day).add(Duration(days: 1))]
+      .add(test4);
+
+  dailyHabit[DateTime(now.year, now.month, now.day).add(Duration(days: 2))] =
+      [];
+  print(
+      'dailyHabit : ${dailyHabit[DateTime(now.year, now.month, now.day).add(Duration(days: 2))]}');
+  dailyHabit.forEach((key, value) {
+    value.forEach((element) {
+      print('element: ${element.name}');
+    });
+  });
+
+  print('daily: $dailyHabit');
 
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
