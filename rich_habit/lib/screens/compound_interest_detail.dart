5353a89a9f1da22725f2e1a7116b658a8cc7077b
Ryusim <bkryusim@gmail.com>
2020-08-23 15:05:48 +0900

    can read History

--- rich_habit/lib/screens/compound_interest_detail.dart
+++ rich_habit/lib/screens/compound_interest_detail.dart
@@ -22,29 +22,27 @@ class _CompoundInterestDetailState extends State<CompoundInterestDetail> {
   void initState() {
     super.initState();
     addedTimeID = widget.addedTimeID;
-    // history = context.read<HabitProvider>().showCumSavingHistory(null);
-    context.read<HabitProvider>().showCumSavingHistory(null);
+    history = context.read<HabitProvider>().showCumSavingHistory(addedTimeID);
+    print(history);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kPurpleColor,
-      appBar: AppBar(
-          backgroundColor: kPurpleColor,
-          brightness: Brightness.light,
-          elevation: 0.0,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios),
-            color: kWhiteIvoryColor,
-            iconSize: kSubTitleFontSize,
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          )),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+        backgroundColor: kPurpleColor,
+        appBar: AppBar(
+            backgroundColor: kPurpleColor,
+            brightness: Brightness.light,
+            elevation: 0.0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              color: kWhiteIvoryColor,
+              iconSize: kSubTitleFontSize,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
           SizedBox(
             height: 30,
             width: double.infinity,
@@ -67,7 +65,7 @@ class _CompoundInterestDetailState extends State<CompoundInterestDetail> {
             height: 15,
           ),
           Text(
-            "${context.watch<HabitProvider>().cumSaving(addedTimeID).toStringAsFixed(0).replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}원",
+            "${context.watch<HabitProvider>().cumSaving(addedTimeID).toStringAsFixed(1).replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}원",
             style: TextStyle(
                 color: kWhiteIvoryColor,
                 fontSize: 25.0,
@@ -77,87 +75,120 @@ class _CompoundInterestDetailState extends State<CompoundInterestDetail> {
             height: 50,
           ),
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: kIvoryColor,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(19)),
-              ),
-              width: double.infinity,
-              height: 80,
-              child: SingleChildScrollView(
-                child: Column(children: [
-                  ConfigurableExpansionTile(
-                    header: Text("Head"),
-                    children: [
-                      Text("HIHI"),
-                      Text("HIHI"),
-                      Text("HIHI"),
-                    ],
-                  ),
-                  ConfigurableExpansionTile(
-                    header: Text("Head"),
-                    children: [
-                      Text("HIHI"),
-                      Text("HIHI"),
-                      Text("HIHI"),
-                    ],
-                  ),
-                  ConfigurableExpansionTile(
-                    header: Text("Head"),
-                    children: [
-                      Text("HIHI"),
-                      Text("HIHI"),
-                      Text("HIHI"),
-                    ],
-                  ),
-                  ConfigurableExpansionTile(
-                    header: Row(children: [
-                      CircleAvatar(
-                        child: Icon(
-                          Icons.keyboard_arrow_up,
-                          color: kWhiteIvoryColor,
-                        ),
-                        backgroundColor: Colors.orange[700],
-                        radius: 12,
-                      ),
-                      Text("Head"),
-                    ]),
-                    children: [
-                      Text("HIHI"),
-                      Text("HIHI"),
-                      Text("HIHI"),
-                    ],
-                  ),
-                  ConfigurableExpansionTile(
-                    header: Text("Head"),
-                    children: [
-                      Text("HIHI"),
-                      Text("HIHI"),
-                      Text("HIHI"),
-                    ],
-                  ),
-                  ConfigurableExpansionTile(
-                    header: Text("Head"),
-                    children: [
-                      Text("HIHI"),
-                      Text("HIHI"),
-                      Text("HIHI"),
-                    ],
-                  ),
-                  ConfigurableExpansionTile(
-                    header: Text("Head"),
-                    children: [
-                      Text("HIHI"),
-                      Text("HIHI"),
-                      Text("HIHI"),
-                    ],
-                  ),
-                ]),
-              ),
+              child: Container(
+            padding: EdgeInsets.only(top: 25.0),
+            decoration: BoxDecoration(
+              color: kIvoryColor,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(19)),
             ),
-          )
-        ],
-      ),
-    );
+            width: double.infinity,
+            height: 80,
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: _showHitsoryList(history)),
+            ),
+          ))
+        ]));
   }
+}
+
+List<Widget> _showHitsoryList(Map<int, dynamic> history) {
+  double tabsize = 16;
+  List<Widget> result = [];
+  Map<int, double> yearvalue = new Map();
+
+  history.forEach((year, weeklist) {
+    List<Widget> yearchildlist = [];
+    yearvalue[year] = 0;
+
+    weeklist.forEach((week, habitlist) {
+      List<Widget> weekchildlist = [];
+      yearvalue[year] += habitlist[0];
+
+      habitlist[1].forEach((row) {
+        String name = row[0];
+        double value = row[1];
+
+        weekchildlist.add(Row(
+          children: [
+            SizedBox(
+              width: tabsize * 2,
+            ),
+            Text(
+              "-  $name",
+              style: TextStyle(
+                  color: kDarkPurpleColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text("$value원",
+                style: TextStyle(color: kDarkPurpleColor, fontSize: 16))
+          ],
+        ));
+      });
+
+      yearchildlist.add(ConfigurableExpansionTile(
+        header: Row(
+          children: [
+            SizedBox(
+              width: tabsize * 1,
+            ),
+            _toggleButton(false),
+            Text(week.toString() + "주",
+                style: TextStyle(
+                    color: kDarkPurpleColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold)),
+            Text(
+                "${habitlist[0].toStringAsFixed(1).replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}원",
+                style: TextStyle(color: kDarkPurpleColor, fontSize: 16)),
+          ],
+        ),
+        children: weekchildlist,
+      ));
+    });
+    result.add(ConfigurableExpansionTile(
+      header: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        _toggleButton(false),
+        Text(
+          "$year년",
+          style: TextStyle(
+              color: kDarkPurpleColor,
+              fontSize: 16,
+              fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "${yearvalue[year].toStringAsFixed(1).replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}원",
+          style: TextStyle(color: kDarkPurpleColor, fontSize: 16),
+        ),
+      ]),
+      children: yearchildlist.reversed.toList(),
+    ));
+  });
+
+  return result.reversed.toList();
+}
+
+CircleAvatar _toggleButton(bool opened) {
+  double rad = 11.5;
+
+  return opened
+      ? CircleAvatar(
+          child: Icon(
+            Icons.keyboard_arrow_up,
+            color: kWhiteIvoryColor,
+          ),
+          backgroundColor: Colors.orange[700],
+          radius: rad,
+        )
+      : CircleAvatar(
+          child: Icon(
+            Icons.keyboard_arrow_down,
+            color: kWhiteIvoryColor,
+          ),
+          backgroundColor: Colors.grey,
+          radius: rad,
+        );
 }
