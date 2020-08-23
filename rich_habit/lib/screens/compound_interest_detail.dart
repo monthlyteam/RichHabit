1271ed33d775f64720824bf9250fab067893c784
Ryusim <bkryusim@gmail.com>
2020-08-23 15:23:01 +0900

    Set Alignment to left. Modified configurableexpansiontile from github fork.

--- rich_habit/lib/screens/compound_interest_detail.dart
+++ rich_habit/lib/screens/compound_interest_detail.dart
@@ -76,7 +76,7 @@ class _CompoundInterestDetailState extends State<CompoundInterestDetail> {
           ),
           Expanded(
               child: Container(
-            padding: EdgeInsets.only(top: 25.0),
+            padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
             decoration: BoxDecoration(
               color: kIvoryColor,
               borderRadius: BorderRadius.vertical(top: Radius.circular(19)),
@@ -95,7 +95,7 @@ class _CompoundInterestDetailState extends State<CompoundInterestDetail> {
 }
 
 List<Widget> _showHitsoryList(Map<int, dynamic> history) {
-  double tabsize = 16;
+  double tabsize = 30;
   List<Widget> result = [];
   Map<int, double> yearvalue = new Map();
 
@@ -117,26 +117,43 @@ List<Widget> _showHitsoryList(Map<int, dynamic> history) {
               width: tabsize * 2,
             ),
             Text(
-              "-  $name",
+              "-  $name  ",
               style: TextStyle(
                   color: kDarkPurpleColor,
                   fontSize: 16,
                   fontWeight: FontWeight.bold),
             ),
-            Text("$value원",
+            Text(
+                "$value.toStringAsFixed(1).replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}원",
                 style: TextStyle(color: kDarkPurpleColor, fontSize: 16))
           ],
         ));
       });
 
       yearchildlist.add(ConfigurableExpansionTile(
+        headerExpanded: Row(
+          children: [
+            SizedBox(
+              width: tabsize * 1,
+            ),
+            _toggleButton(true),
+            Text("   $week주  ",
+                style: TextStyle(
+                    color: kDarkPurpleColor,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold)),
+            Text(
+                "${habitlist[0].toStringAsFixed(1).replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}원",
+                style: TextStyle(color: kDarkPurpleColor, fontSize: 16)),
+          ],
+        ),
         header: Row(
           children: [
             SizedBox(
               width: tabsize * 1,
             ),
             _toggleButton(false),
-            Text(week.toString() + "주",
+            Text("   $week주  ",
                 style: TextStyle(
                     color: kDarkPurpleColor,
                     fontSize: 16,
@@ -150,10 +167,25 @@ List<Widget> _showHitsoryList(Map<int, dynamic> history) {
       ));
     });
     result.add(ConfigurableExpansionTile(
+      headerExpanded:
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        _toggleButton(true),
+        Text(
+          "   $year년  ",
+          style: TextStyle(
+              color: kDarkPurpleColor,
+              fontSize: 16,
+              fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "${yearvalue[year].toStringAsFixed(1).replaceAllMapped(new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}원",
+          style: TextStyle(color: kDarkPurpleColor, fontSize: 16),
+        ),
+      ]),
       header: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
         _toggleButton(false),
         Text(
-          "$year년",
+          "   $year년  ",
           style: TextStyle(
               color: kDarkPurpleColor,
               fontSize: 16,
@@ -172,7 +204,7 @@ List<Widget> _showHitsoryList(Map<int, dynamic> history) {
 }
 
 CircleAvatar _toggleButton(bool opened) {
-  double rad = 11.5;
+  double rad = 12;
 
   return opened
       ? CircleAvatar(
