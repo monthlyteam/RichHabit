a3874b0f262d05ebe5766863dfc90116010241c9
Ryusim <bkryusim@gmail.com>
2020-08-24 16:40:52 +0900

    localnoti in userprovider

--- rich_habit/lib/user_provider.dart
+++ rich_habit/lib/user_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class UserProvider with ChangeNotifier {
   bool isLogin = false;
@@ -12,4 +13,45 @@ class UserProvider with ChangeNotifier {
   DateTime pushAlarmTime;
 
   String language = 'KO';
+
+  FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  UserProvider() {
+    //Android
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    //IOS
+    var initializationSettingsIOS = IOSInitializationSettings();
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    //for when notification pressed.
+    //_flutterLocalNotificationsPlugin.initialize(initializationSettings,
+    //    onSelectNotification: onSelectNotification);
+  }
+
+  Future<void> setTriggerNotification() async {
+    Time time =
+        Time(pushAlarmTime.hour, pushAlarmTime.minute, pushAlarmTime.second);
+
+    var android = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+
+    var ios = IOSNotificationDetails();
+    var detail = NotificationDetails(android, ios);
+
+    await _flutterLocalNotificationsPlugin.showDailyAtTime(
+      0,
+      'RichHabit',
+      '빨리 양치해!',
+      time,
+      detail,
+      payload: 'trigger',
+    );
+  }
 }
